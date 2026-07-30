4ce18641e468fe65cbdad58bf6a6b31068668d1d
ViniciusDCDias <viniciusdcenzi@gmail.com>
2026-07-30 20:07:01 -0300

    feat: Rota POST /cardapio

--- backend/src/routes/cardapio.routes.js
+++ backend/src/routes/cardapio.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
+import { createRefeicao } from "../controllers/cardapio.controller.js";
+
 const router = Router()
+
+router.post("/",authMiddleware,createRefeicao)
 
 export default router
