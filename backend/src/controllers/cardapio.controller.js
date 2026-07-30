ec2545297fa859b35b3bd608cc805454ac2d783e
ViniciusDCDias <viniciusdcenzi@gmail.com>
2026-07-30 20:21:26 -0300

    feat: Rota PUT /cardapio/:data

--- backend/src/controllers/cardapio.controller.js
+++ backend/src/controllers/cardapio.controller.js
@@ -1,20 +1,20 @@
 import prisma from "../lib/prisma"
 export async function createRefeicao(req, res) {
     try {
-        const tiposPerm = ["GESTOR"];
+        const tiposPerm = ["GESTOR"]
         if (!tiposPerm.includes(req.user.tipo)) {
-            return res.status(403).json({ message: "Tipo de usuário não autorizado, tente novamente..." });
+            return res.status(403).json({ message: "Tipo de usuário não autorizado, tente novamente..." })
         }
         const refeicao = req.body.novo;
         if (!refeicao) {
-            return res.status(400).json({ message: "Dados para refeição não enviados, tente novamente." });
+            return res.status(400).json({ message: "Dados para refeição não enviados, tente novamente." })
         }
-        const camposObrigatorios = ['nome', 'descricao', 'data'];
-        const campoFaltando = camposObrigatorios.find(campo => !refeicao[campo]);
+        const camposObrigatorios = ['nome', 'descricao', 'data']
+        const campoFaltando = camposObrigatorios.find(campo => !refeicao[campo])
         if (campoFaltando) {
             return res.status(400).json({ 
                 message: `O campo '${campoFaltando}' é obrigatório para cadastrar a refeição.` 
-            });
+            })
         }
         const novaRefeicao = await prisma.cardapio.create({
             data: {
@@ -22,13 +22,13 @@ export async function createRefeicao(req, res) {
                 descricao_ref: refeicao.descricao,
                 data_ref: new Date(`${refeicao.data}T00:00:00.000Z`)
             }
-        });
+        })
         return res.status(201).json({
             message: "Refeição criada com sucesso!"
-        });
+        })
     } catch (error) {
-        console.error("Erro ao criar refeição:", error);
-        return res.status(500).json({ message: "Erro interno no servidor ao cadastrar refeição." });
+        console.error("Erro ao criar refeição:", error)
+        return res.status(500).json({ message: "Erro interno no servidor ao cadastrar refeição." })
     }
 }
 
@@ -36,24 +36,54 @@ export async function excludeRefeicao(req, res) {
     try {
         const tiposPerm = ["GESTOR"];
         if (!tiposPerm.includes(req.user.tipo)) {
-            return res.status(403).json({ message: "Tipo de usuário não autorizado, tente novamente..." });
+            return res.status(403).json({ message: "Tipo de usuário não autorizado, tente novamente..." })
         }
-        const { data } = req.params;
+        const { data } = req.params
         if (!data) {
-            return res.status(400).json({ message: "A data não foi enviada, tente novamente." });
+            return res.status(400).json({ message: "A data não foi enviada, tente novamente." })
         }
-        const dataFormatada = new Date(`${data}T00:00:00.000Z`);
+        const dataFormatada = new Date(`${data}T00:00:00.000Z`)
         await prisma.cardapios.delete({
             where: {
                 data_ref: dataFormatada
             }
-        });
-        return res.status(200).json({ message: "Refeição excluída com sucesso!" });
+        })
+        return res.status(200).json({ message: "Refeição excluída com sucesso!" })
     } catch (error) {
         if (error.code === 'P2025') {
-            return res.status(404).json({ message: "Nenhuma refeição encontrada para esta data." });
+            return res.status(404).json({ message: "Nenhuma refeição encontrada para esta data." })
         }
-        console.error("Erro ao excluir refeição:", error);
-        return res.status(500).json({ message: "Erro interno no Servidor" });
+        console.error("Erro ao excluir refeição:", error)
+        return res.status(500).json({ message: "Erro interno no Servidor" })
+    }
+}
+
+export async function updateRefeicao(req,res){
+    try{
+        const tiposPerm = ["GESTOR"];
+        if (!tiposPerm.includes(req.user.tipo)) {
+            return res.status(403).json({ message: "Tipo de usuário não autorizado, tente novamente..." });
+        }
+        const {data} = req.params
+        const descricao = req.body.descricao
+        if(!data || !descricao){
+            return res.status(400).json({message:"Informações obridatorias não foram enviadas, tente novamente."})
+        }
+        const dataFormatada = new Date(`${data}T00:00:00.000Z`)
+        await prisma.cardapios.update({
+            where: {
+                data_ref:dataFormatada
+            },
+            data:{
+                descricao_ref:descricao
+            }
+        })
+        return res.status(200).json({message:"Refeição excluida com sucesso!"})
+    }catch(error){
+        if (error.code === 'P2025') {
+            return res.status(404).json({ message: "Nenhuma refeição encontrada para esta data." })
+        }
+        console.log(error)
+        return res.status(500).json({message:"Erro Interno no Servidor"})
     }
 }
--- backend/src/routes/cardapio.routes.js
+++ backend/src/routes/cardapio.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
-import { createRefeicao, excludeRefeicao } from "../controllers/cardapio.controller.js";
+import { createRefeicao, excludeRefeicao, updateRefeicao } from "../controllers/cardapio.controller.js";
 
 const router = Router()
 
 router.post("/",authMiddleware,createRefeicao)
 router.delete("/:data",authMiddleware,excludeRefeicao)
+router.put("/:data",authMiddleware,updateRefeicao)
 
 export default router
